9635b3bef9891098fe87bcbf2ff958cdfdb989a9
yrachid <anuar.yasser@gmail.com>
2020-06-11 00:49:09 -0300

    Cria classe Livro

---
+++ src/main/java/com/aceleradora/biblioteca/Livro.java
@@ -0,0 +1,13 @@
+package com.aceleradora.biblioteca;
+
+class Livro {
+    private String titulo;
+
+    Livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    String getTitulo() {
+        return titulo;
+    }
+}
--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     private static final String BOAS_VINDAS = "Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre.";
 
-    private static final String[] LIVROS = new String[]{
-            "Program Development in Java",
-            "Use a cabeça: Padrões de projeto"
+    private static final Livro[] LIVROS = new Livro[]{
+            new Livro("Program Development in Java"),
+            new Livro("Use a cabeça: Padrões de projeto")
     };
 
     public static void main(String[] args) {
         System.out.println(BOAS_VINDAS);
 
         System.out.println("Livros Disponiveis:");
-        for (String livro : LIVROS) {
-            System.out.printf("- %s\n", livro);
+        for (Livro livro : LIVROS) {
+            System.out.printf("- %s\n", livro.getTitulo());
         }
     }
 }
