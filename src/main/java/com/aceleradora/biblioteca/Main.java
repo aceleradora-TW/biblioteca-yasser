4ecfcd73df770a7a20410ae8be30e35f5102a381
yrachid <anuar.yasser@gmail.com>
2020-06-11 00:46:03 -0300

    Extrai livros para uma constante

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     private static final String BOAS_VINDAS = "Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre.";
 
+    private static final String[] LIVROS = new String[]{
+            "Program Development in Java",
+            "Use a cabeça: Padrões de projeto"
+    };
+
     public static void main(String[] args) {
         System.out.println(BOAS_VINDAS);
 
         System.out.println("Livros Disponiveis:");
-        System.out.println("- Program Development in Java");
-        System.out.println("- Use a cabeça: Padrões de projeto");
+        for (String livro : LIVROS) {
+            System.out.printf("- %s\n", livro);
+        }
     }
 }
