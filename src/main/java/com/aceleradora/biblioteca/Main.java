d17b850d0a22b9f80e4abb756089a293134829d9
yrachid <anuar.yasser@gmail.com>
2020-06-11 00:42:09 -0300

    Mostra mensagem de boas vindas

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -2,7 +2,9 @@ package com.aceleradora.biblioteca;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String BOAS_VINDAS = "Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre.";
 
+    public static void main(String[] args) {
+        System.out.println(BOAS_VINDAS);
     }
 }
